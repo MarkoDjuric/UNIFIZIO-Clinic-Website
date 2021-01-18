58f318c46bc98057c9ebfcd93eb2484c13e2f61f
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2021-01-18 23:33:37 +0100

    Update app.js

--- app.js
+++ app.js
@@ -178,3 +178,21 @@ dottBtns.forEach(element => { element.addEventListener('click', () => {
    })();
    
 
+
+
+   // Smooth scrolling
+   const makeNavLinksSmooth = () => {
+      const navLinks = document.querySelectorAll('.navbar__link');
+
+      for (let n in navLinks) {
+         if(navLinks.hasOwnProperty(n)){
+            navLinks[n].addEventListener('click', e => {
+               e.preventDefault();
+               document.querySelector(navLinks[n].hash).scrollIntoView({
+                  behavior: "smooth"
+               });
+
+            });
+         }
+      }
+   }
